e189a1491112279f6aa48ea7d52d0877ec6074b4
Robert Claypool <robert.g.claypool@gmail.com>
2020-06-04 21:57:09 -0500

    Add our second question, switch to async/await

--- index.js
+++ index.js
@@ -2,23 +2,39 @@ const inquirer = require("inquirer");
 
 console.log("Thank you for trying Edutainment Africa.");
 
-inquirer
-  .prompt([
-    {
-      type: "confirm",
-      name: "continue",
-      message: "To continue, press Y. Press any other key to exit.",
-    },
-  ])
-  .then((answers) => {
-    console.log(`You answered: '${answers["continue"]}'. Thank you.`);
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-      console.log("Error. Environment not supported.");
-    } else {
-      // Something else when wrong
-      console.log("Error. Unable to continue.");
-    }
-  });
+async function main() {
+  let answer = await inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "continue",
+        message: "To continue, press Y. Press any other key to exit.",
+      },
+    ])
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        console.log("Error. Environment not supported.");
+      } else {
+        // Something else when wrong
+        console.log("Error. Unable to continue.");
+      }
+    });
+
+  if (answer["continue"] === true) {
+    answer = await inquirer.prompt([
+      {
+        type: "list",
+        name: "country",
+        message: "In which country would you like to start your tour?",
+        choices: ["Botswana", "Egypt", "Ethiopia", "Guinea", "Nigeria"],
+      },
+    ]);
+  } else {
+    console.log("Good bye.");
+  }
+
+  console.log(`${answer["country"]} is a great choice. Let's go!`);
+}
+
+main();
